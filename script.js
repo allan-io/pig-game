3f6a467f1cb76aaeadc5da60fe984fcbfba2d02c
Allan Oliveira <Allan@Allans-MacBook-Pro.local>
2022-01-02 12:53:09 -0500

    added active and inactive game states

--- script.js
+++ script.js
@@ -7,8 +7,6 @@ const btnNew = document.querySelector('.btn--new');
 const btnRoll = document.querySelector('.btn--roll');
 const btnHold = document.querySelector('.btn--hold');
 
-// game has two states, active/ not active
-
 score0El.textContent = 0;
 score1El.textContent = 0;
 
@@ -19,6 +17,7 @@ let gameActive = true;
 
 diceEl.classList.add('hidden');
 
+// REFACTOR CODE TODO
 const changePlayer = function () {
   document.querySelector('#current--' + currentPlayer).textContent = 0;
   currentPlayer === 0 ? (currentPlayer = 1) : (currentPlayer = 0);
@@ -30,31 +29,34 @@ const changePlayer = function () {
 };
 
 btnRoll.addEventListener('click', function () {
-  const randNum = Math.trunc(Math.random() * 6) + 1;
-  diceEl.src = `dice-${randNum}.png`;
-  diceEl.classList.remove('hidden');
-  tempScore += randNum;
-  const currentScore = document.querySelector('#current--' + currentPlayer);
-  currentScore.textContent = tempScore;
-  if (randNum === 1) {
-    changePlayer();
+  if (gameActive) {
+    const randNum = Math.trunc(Math.random() * 6) + 1;
+    diceEl.src = `dice-${randNum}.png`;
+    diceEl.classList.remove('hidden');
+    tempScore += randNum;
+    const currentScore = document.querySelector('#current--' + currentPlayer);
+    currentScore.textContent = tempScore;
+    if (randNum === 1) {
+      changePlayer();
+    }
   }
 });
 
-// hold button TODO
 btnHold.addEventListener('click', function () {
-  totalScore[currentPlayer] += tempScore;
-  currentPlayer === 0
-    ? (score0El.textContent = totalScore[0])
-    : (score1El.textContent = totalScore[1]);
-  // add class, change state of game to inactive TODO
-  if (totalScore[currentPlayer] >= 100) {
-    diceEl.classList.add('hidden');
-    document
-      .querySelector('.player--' + currentPlayer)
-      .classList.add('player--winner');
+  if (gameActive) {
+    totalScore[currentPlayer] += tempScore;
+    currentPlayer === 0
+      ? (score0El.textContent = totalScore[0])
+      : (score1El.textContent = totalScore[1]);
+    if (totalScore[currentPlayer] >= 20) {
+      gameActive = false;
+      diceEl.classList.add('hidden');
+      document
+        .querySelector('.player--' + currentPlayer)
+        .classList.add('player--winner');
+    }
+    changePlayer();
   }
-  changePlayer();
 });
 
 btnNew.addEventListener('click', function () {
@@ -73,4 +75,5 @@ btnNew.addEventListener('click', function () {
   document.querySelector('#current--1').textContent = 0;
   score0El.textContent = 0;
   score1El.textContent = 0;
+  gameActive = true;
 });
